ce6fb5f1bf8187eb95085610c4bf61403616f9b1
Olcoyote <1021634511@teinco.edu.co>
2025-03-26 10:42:45 -0500

    Add files via upload

---
+++ funcionuno.java
@@ -0,0 +1,12 @@
+//creacion de la funcion sumar dos numertos en java
+class funcionuno {
+
+    public static int sumar(int a , int b){
+    return a + b ;
+    }
+    //ejecucion de la funcion sumar
+    public static void main(String[]args){
+        int resultado = sumar (15 , 333);
+        System.out.println(resultado);
+    }
+}
